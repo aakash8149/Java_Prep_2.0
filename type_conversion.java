9f2d4506183122e70ff9093dc055ad7d8379038a
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-07 14:16:56 +0530

    Add files via upload

---
+++ type_conversion.java
@@ -0,0 +1,12 @@
+public class type_conversion {
+    public static void main(String args[]){
+        float a = 10.15254f;
+        int b = (int)a;
+        System.out.println(b);
+
+
+        char ch = 'a';
+        int s = (int)ch;
+        System.out.println(s);
+    }
+}
