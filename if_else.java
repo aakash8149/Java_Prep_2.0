e600ed25f2d993f806a055f8514fb00266be1cf2
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-10 11:50:17 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,13 @@
+package Conditional_Statements;
+
+public class if_else {
+    public static void main(String[] args) {
+        int age = 50;
+        if (age >= 18) {
+            System.out.println("Yess ! This person is an Adult !");
+        }
+        else{
+            System.out.println("He is not an adult !");
+        }
+    }
+}
