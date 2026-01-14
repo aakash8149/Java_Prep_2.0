ca8130aa4c949cc91e01f7853b4cc109d20e9bf6
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-14 19:08:18 +0530

    Add files via upload

---
+++ largest_number.java
@@ -0,0 +1,13 @@
+package Conditional_Statements;
+
+public class largest_number {
+    public static void main(String[] args) {
+        int a = 1, b = 5;
+
+        if (a>=b) {
+            System.out.println(a + "This is the largest");
+        } else{
+            System.out.println(b + " This is the largest!!");
+        }
+    }
+}
