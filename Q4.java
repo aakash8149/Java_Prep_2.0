602bdc1233175e7b19760687234bfee8bcf40918
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-18 21:41:35 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,13 @@
+package Conditional_Statements;
+//Find the ouput of the program !!!
+public class Q4 {
+    public static void main(String[] args) {
+        int a  = 63, b = 36;
+
+        boolean x = (a<b) ? true : false;
+        int y = (a>b) ? a:b;
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
