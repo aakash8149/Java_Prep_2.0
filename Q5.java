22079718af01163be7847f4fa0425514704efc7e
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-18 21:41:53 +0530

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,27 @@
+package Conditional_Statements;
+//Leap Year or not !!
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Year: ");
+        int year = sc.nextInt();
+
+        boolean x = (year%4) == 0;
+        boolean y = (year%100) != 0;
+        boolean z = ((year%100 == 0) && (year%400 == 0));
+
+        if (x && (y || z)) {
+            System.out.println("Leap Year !!");
+        }
+        else{
+            System.out.println("Not a leap Year !!");
+        }
+        sc.close();
+
+
+    }
+    
+}
