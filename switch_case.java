620be546cd161b713ed86813be999ee7b2d9e5d6
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-26 21:31:03 +0530

    Add files via upload

---
+++ switch_case.java
@@ -0,0 +1,18 @@
+package Conditional_Statements;
+
+public class switch_case {
+    public static void main(String[] args) {
+        int number = 2; // We should take input here !
+
+        switch (number) {
+            case 1: System.out.println("Samosa Laoo");
+                break;
+            case 2: System.out.println("Pakoda Laaoo");
+                break;
+            case 3: System.out.println("Burger Laaooo");
+                break;
+            default: System.out.println("No Order !!");
+                break;
+        }
+    }
+}
