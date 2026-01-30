22e92e4a5a2957df6dcbd4a03f44ace7e03b4e63
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-30 12:59:22 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,12 @@
+package Flow_Controls_Loops;
+
+public class do_while_loop {
+    public static void main(String[] args) {
+        
+    int a = 1;
+    do{
+        System.out.println("Hello World");
+        a++;
+    } while(a<=10);
+    }
+}
