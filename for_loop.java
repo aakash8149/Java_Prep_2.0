8b872d572fbe3c43ac3ae6b13bcb89aaf3b5c7b3
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-30 13:00:19 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,15 @@
+package Flow_Controls_Loops;
+
+public class for_loop {
+
+    /*for(initialisation; Condition; Updation){
+        //Write Here
+    } 
+    */
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+        System.out.println("Hello World !");
+    }
+    }
+    
+}
