2feececfa0c47e732d44041b83810720a76c5a05
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-01-31 13:15:51 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,13 @@
+package Flow_Controls_Loops;
+
+public class loops {
+    public static void main(String[] args) {
+        int n = 0;
+
+        while (n<100) {
+            System.out.println("Hello World !!");
+            n++;
+            System.out.println(n);
+        }
+    }
+}
