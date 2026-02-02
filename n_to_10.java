d3934c424ef1c8a5e5659bf0d25461b371145fdd
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-02-02 12:02:27 +0530

    Add files via upload

---
+++ n_to_10.java
@@ -0,0 +1,11 @@
+package Flow_Controls_Loops;
+
+public class n_to_10 {
+    public static void main(String args[]){
+        int a = 1;
+        while (a<=10) {
+            System.out.print(a+" ");
+            a++;
+        }
+    }
+}
