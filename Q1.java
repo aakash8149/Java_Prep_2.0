cba99d4e06c19475e97dff84ed0dfb0a3b622cb8
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-02-03 15:23:17 +0530

    Add files via upload

--- Q1.java
+++ Q1.java
@@ -1,16 +1,12 @@
-package Conditional_Statements;
-
-import java.util.Scanner;
-
-//Write a Java program to get a number from the user and print whether it is positive or negative
+package Flow_Controls_Loops;
+// How many times the Hello World is Printed ?
 public class Q1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the Number: ");
-        int num = sc.nextInt();
-
-        String ans = (num>=0 ? "Positive" : "Negative");
-        System.out.println(ans);
-        sc.close();
+        for(int i=0; i<5; i++){
+            System.out.println("Hello World");
+            i += 2;
+        }
     }
 }
+
+//Answer: 2 Times
