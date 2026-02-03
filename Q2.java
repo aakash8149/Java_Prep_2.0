ae2623fbe5ec8d792b9b7c503555a0e730c078ed
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-02-03 15:26:35 +0530

    Add files via upload

--- Q2.java
+++ Q2.java
@@ -1,20 +1,28 @@
-package Conditional_Statements;
+package Flow_Controls_Loops;
 
 import java.util.Scanner;
 
-//Question: Has Fever or not !!
+//Write a program that reads a set of integers, and then prints the sum of the even and odd integers...
 public class Q2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Temperature: ");
-        double temp = sc.nextDouble();
+        Scanner sc = new Scanner(System.in); int number, even_sum = 0, odd_sum = 0; char choice;
 
-        if (temp>100) {
-            System.out.println("You have Fever !! Take rest !!");
-        }
-        else{
-            System.out.println("You don't have any fever, u r overacting !!!");
-        }
+        do{
+            System.out.print("Enter the Number: ");
+            number = sc.nextInt();
+
+            if (number % 2 == 0) {
+                even_sum += number;
+            }else{
+                odd_sum += number;
+            }
+
+            System.out.print("Do you do want to Continue {Press Y/N}: ");
+            choice = sc.next().charAt(0);
+        } while(choice == 'Y' || choice == 'y');
+
+        System.out.println("The Even Sum is : " + even_sum);
+        System.out.println("The Odd Sum is : " + odd_sum);
         sc.close();
 
     }
