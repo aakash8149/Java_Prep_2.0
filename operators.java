be12a563bcc56bdd2ffadd63a73167797998dbba
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-02-12 19:53:19 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class operators {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+        System.out.println(++A);
+        System.out.println(--B);
+    }
+}
