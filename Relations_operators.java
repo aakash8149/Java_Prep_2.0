a3e9536913537996456e40af9a2ea25e5307ded2
Aakash Das <108335757+aakash8149@users.noreply.github.com>
2026-02-12 19:53:49 +0530

    Add files via upload

---
+++ Relations_operators.java
@@ -0,0 +1,31 @@
+package Operators;
+
+public class Relations_operators {
+    public static void main(String[] args) {
+        // int a = 80;
+        // int b = 80;
+
+        // if (a==b) {
+        //     System.out.println("Same !");
+        // }
+        // else{
+        //     System.out.println("Different !");
+        // }
+        
+        // System.out.println(a==b);
+        // System.out.println(a!=b);
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+        // System.out.println(a<=b);
+        // System.out.println(a>=b);
+        int x = 2, y = 5;
+        
+        int exp1 = (x * y / x);
+        int exp2 = (x * (y / x));
+        System.out.print(exp1 + ",");
+        System.out.print(exp2);
+
+
+
+    }
+}
